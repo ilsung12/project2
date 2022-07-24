a18c6130d851284e1ccbd0d9755031e827156d53
coil <75682926+ilsung12@users.noreply.github.com>
2022-07-24 21:19:24 +0900

    🌱 Init: Install and Settings

---
+++ vuetify-admin-template/src/router/index.js
@@ -0,0 +1,28 @@
+// router -> 경로 설정 내용
+
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Dashboard from '@/views/Dashboard';
+import GridSystem from '@/views/GridSystem';
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/',
+    name: 'Dashboard',
+    component: Dashboard,
+  },
+  {
+    path: '/grid-system',
+    name: 'GridSystem',
+    component: GridSystem,
+  },
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  routes,
+});
+
+export default router;
---
+++ vuetify-admin-template/src/views/GridSystem.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Grid System Page
+  </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
